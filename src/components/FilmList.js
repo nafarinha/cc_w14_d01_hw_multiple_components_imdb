cffcc349903579f51aa8f29efb97d7d9187fa147
Nuno Farinha <nadfarinha@gmail.com>
2019-04-15 22:04:14 +0100

    Create film list component

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
+import Film from './Film';
 
 class FilmList extends Component{
+  render() {
+    const filmNodes = this.props.data.map(film => {
+      return (
+        <Film url={film.url} key={film.id}>{film.name}</Film>
+      )
+    })
+
+    return (
+      <ul className="film-list">
+        {filmNodes}
+      </ul>
+    )
+  }
 
 }
 
